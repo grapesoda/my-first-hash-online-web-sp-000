0e31f5117f6d1c870ec2fd78a18cf52b1f705f10
Tres Tarwater <trest03@gmail.com>
2020-12-08 20:16:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,7 +2,7 @@ def my_hash
   
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
-my_hash = {"name" => "tres", "nickname" => "3"} 
+your_hash = {"name" => "tres", "nickname" => "3"} 
   
 end
 
